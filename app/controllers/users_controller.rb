ddca6afcfc7735f9a74aa4d66f3802c66cf196df
The Tiến <tiendt2311@gmail.com>
2017-04-06 17:06:21 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+
+  # GET
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+  # GET
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User create successful"
+      redirect_to @user
+    else
+      flash.now[:alert] = "Error !"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
 
@@ -12,5 +11,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get '/singup', to: 'users#new'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
