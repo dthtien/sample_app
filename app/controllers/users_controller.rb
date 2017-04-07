8d4b88a1c30d20f06637c22f194dcf09e76f1ac2
The Tiến <tiendt2311@gmail.com>
2017-04-07 22:23:21 +0700

    Test commit to Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:notice] = "User create successful"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   get '/help', to: 'static_pages#help'
@@ -10,8 +12,14 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
-  get '/singup', to: 'users#new'
+  get '/signup', to: 'users#new'
+
 
   resources :users
+  
+  resources :session, only: [:new, :create, :destroy]
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
